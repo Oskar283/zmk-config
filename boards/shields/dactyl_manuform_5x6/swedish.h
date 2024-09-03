deccfe5fc5418c89dd74c930c2d2026b9ec8e74c
Oskar <oskar@oskar>
2024-09-03 20:48:09 +0200

    Add swedish headerfile

---
+++ boards/shields/dactyl_manuform_5x6/swedish.h
@@ -0,0 +1,138 @@
+
+#pragma once
+
+#include "keymap.h"
+
+// clang-format off
+
+/*
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
+ * │ § │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ + │ ´ │       │
+ * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
+ * │     │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ Å │ ¨ │     │
+ * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    │
+ * │      │ A │ S │ D │ F │ G │ H │ J │ K │ L │ Ö │ Ä │ ' │    │
+ * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤
+ * │    │ < │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ - │          │
+ * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
+ * │    │    │    │                        │    │    │    │    │
+ * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
+ */
+// Row 1
+#define SE_SECT GRAVE  // §
+#define SE_1    N1    // 1
+#define SE_2    N2    // 2
+#define SE_3    N3    // 3
+#define SE_4    N4    // 4
+#define SE_5    N5    // 5
+#define SE_6    N6    // 6
+#define SE_7    N7    // 7
+#define SE_8    N8    // 8
+#define SE_9    N9    // 9
+#define SE_0    N0    // 0
+#define SE_PLUS MINUS // +
+#define SE_ACUT EQUAL  // ´ (dead)
+// Row 2
+#define SE_Q    Q    // Q
+#define SE_W    W    // W
+#define SE_E    E    // E
+#define SE_R    R    // R
+#define SE_T    T    // T
+#define SE_Y    Y    // Y
+#define SE_U    U    // U
+#define SE_I    I    // I
+#define SE_O    O    // O
+#define SE_P    P    // P
+#define SE_AA LEFT_BRACKET // Å
+#define SE_DIAE RBRC // ¨ (dead)
+// Row 3
+#define SE_A    A    // A
+#define SE_S    S    // S
+#define SE_D    D    // D
+#define SE_F    F    // F
+#define SE_G    G    // G
+#define SE_H    H    // H
+#define SE_J    J    // J
+#define SE_K    K    // K
+#define SE_L    L    // L
+#define SE_OE SEMICOLON // Ö
+#define SE_AE SINGLE_QUOTE // Ä
+#define SE_QUOT NUHS // '
+// Row 4
+#define SE_LABK NUBS // <
+#define SE_Z    Z    // Z
+#define SE_X    X    // X
+#define SE_C    C    // C
+#define SE_V    V    // V
+#define SE_B    B    // B
+#define SE_N    N    // N
+#define SE_M    M    // M
+#define SE_COMMA COMMA // ,
+#define SE_DOT  DOT  // .
+#define SE_MINUS FSLH // -
+
+/* Shifted symbols
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
+ * │ ½ │ ! │ " │ # │ ¤ │ % │ & │ / │ ( │ ) │ = │ ? │ ` │       │
+ * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
+ * │     │   │   │   │   │   │   │   │   │   │   │   │ ^ │     │
+ * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    │
+ * │      │   │   │   │   │   │   │   │   │   │   │   │ * │    │
+ * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤
+ * │    │ > │   │   │   │   │   │   │   │ ; │ : │ _ │          │
+ * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
+ * │    │    │    │                        │    │    │    │    │
+ * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
+ */
+// Row 1
+#define SE_HALF SHIFT(SE_SECT) // ½
+#define SE_EXLM SHIFT(SE_1)    // !
+#define SE_DQUO SHIFT(SE_2)    // "
+#define SE_HASH SHIFT(SE_3)    // #
+#define SE_CURR SHIFT(SE_4)    // ¤
+#define SE_PERC SHIFT(SE_5)    // %
+#define SE_AMPR SHIFT(SE_6)    // &
+#define SE_SLSH SHIFT(SE_7)    // /
+#define SE_LPRN SHIFT(SE_8)    // (
+#define SE_RPRN SHIFT(SE_9)    // )
+#define SE_EQL  SHIFT(SE_0)    // =
+#define SE_QUES SHIFT(SE_PLUS) // ?
+#define SE_GRV  SHIFT(SE_ACUT) // ` (dead)
+// Row 2
+#define SE_CIRC SHIFT(SE_DIAE) // ^ (dead)
+// Row 3
+#define SE_ASTR SHIFT(SE_QUOT) // *
+// Row 4
+#define SE_RABK SHIFT(SE_LABK) // >
+#define SE_SCLN SHIFT(SE_COMM) // ;
+#define SE_COLN SHIFT(SE_DOT)  // :
+#define SE_UNDS SHIFT(SE_MINS) // _
+
+/* AltGr symbols
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
+ * │   │   │ @ │ £ │ $ │ € │   │ { │ [ │ ] │ } │ \ │   │       │
+ * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
+ * │     │   │   │   │   │   │   │   │   │   │   │   │ ~ │     │
+ * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    │
+ * │      │   │   │   │   │   │   │   │   │   │   │   │   │    │
+ * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤
+ * │    │ | │   │   │   │   │   │   │ µ │   │   │   │          │
+ * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
+ * │    │    │    │                        │    │    │    │    │
+ * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
+ */
+// Row 1
+#define SE_AT   RA(SE_2)    // @
+#define SE_PND  RA(SE_3)    // £
+#define SE_DLR  RA(SE_4)    // $
+#define SE_EURO RA(SE_5)    // €
+#define SE_LCBR RA(SE_7)    // {
+#define SE_LBRC RA(SE_8)    // [
+#define SE_RBRC RA(SE_9)    // ]
+#define SE_RCBR RA(SE_0)    // }
+#define SE_BSLS RA(SE_PLUS) // (backslash)
+// Row 2
+#define SE_TILD RA(SE_DIAE) // ~ (dead)
+// Row 4
+#define SE_PIPE RA(SE_LABK) // |
+#define SE_MICR RA(SE_M)    // µ
